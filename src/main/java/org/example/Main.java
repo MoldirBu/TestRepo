9771f28d4826a806de8d8c1285c5f2a32e318567
Moldir Bupezhanova <moldirbupezhanova@MacBook-Pro-Moldir.local>
2024-02-08 13:38:00 +0600

    hello moldir

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodShowMessageAble();
+        mainMethodShowInfo();
+    }
+    private static void mainMethodShowMessageAble() {
+        System.out.println("Hello moldir svoi tekst\n");
+    }
+    private static void mainMethodShowInfo() {
+        System.out.println("test tekst");
+    }
+}
